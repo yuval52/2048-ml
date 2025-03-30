b600e41d4ca574b548b9131fb03690ae6df78005
yuval52 <yuvaldamti12@gmail.com>
2025-03-31 00:29:38 +0300

    45

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\merav\Documents\GitHub\2048-ml\venv
+set VIRTUAL_ENV=C:\Users\Yuval\Unity Projects\2048-ml\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
